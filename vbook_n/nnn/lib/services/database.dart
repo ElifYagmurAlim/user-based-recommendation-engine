c80feddeafa5da0897b3af0256c5f4b036e284da
Elif Alim <64834857+ElifYagmurAlim@users.noreply.github.com>
2021-11-27 00:57:12 +0300

    last

--- vbook_n/nnn/lib/services/database.dart
+++ vbook_n/nnn/lib/services/database.dart
@@ -6,16 +6,18 @@ class VbookDatabase {
 
   Future<String> createUser(CurrentUser user) async {
     String retVal = "error";
+
     try {
       CollectionReference users =
           FirebaseFirestore.instance.collection('users');
-      users
-          .add({
-            'email': user.email,
-            'userName': user.userName,
-          })
-          .then((value) => print("User Added"))
-          .catchError((error) => print("Failed to add user: $error"));
+      users.add({
+        'email': user.email,
+        'userName': user.userName,
+      });
+      //       FirebaseFirestore.instance.collection('users').add({
+      //   'email': user.email,
+      //   'userName': user.userName,
+      // });
       retVal = 'success';
     } catch (e) {
       print(e);
