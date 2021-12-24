458ed0086ba8bcc5d4a55e08d5581f00bfca555b
Elif Alim <64834857+ElifYagmurAlim@users.noreply.github.com>
2021-12-24 20:08:16 +0300

    4aasd

--- vbook_n/nnn/lib/screens/home/localwidgets/home_form_screen.dart
+++ vbook_n/nnn/lib/screens/home/localwidgets/home_form_screen.dart
@@ -12,50 +12,8 @@ class HomeFormScreen extends StatefulWidget {
 }
 
 class _HomeFormScreenState extends State<HomeFormScreen> {
-<<<<<<< HEAD
-  DatabaseReference dref = FirebaseDatabase.instance.ref();
-  List<Book> books = [];
-  fechData(DataSnapshot x) async {
-//     DatabaseReference ref = FirebaseDatabase.instance.ref();
-    books.add(Book(
-        authors: x.child("authors").value.toString(),
-        average_rating: x.child("average_rating").value.toString(),
-        bookID: x.child("bookID").value.toString(),
-        isbn: x.child("isbn").value.toString(),
-        language_code: x.child("language_code").value.toString(),
-        num_pages: x.child("num_pages").value.toString(),
-        publication_date: x.child("publication_date").value.toString(),
-        publisher: x.child("publisher").value.toString(),
-        ratings_count: x.child("ratings_count").value.toString(),
-        text_reviews_count: x.child("text_reviews_count").value.toString(),
-        title: x.child("title").value.toString()));
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: FirebaseAnimatedList(
-        query: dref,
-        itemBuilder: (BuildContext context, DataSnapshot snapshot,
-            Animation<double> animation, int index) {
-          DataSnapshot x = snapshot;
-          fechData(x);
-          print(books[0].title.toString());
-          return ListTile(
-            subtitle: Text(snapshot.value.toString()),
-          );
-        },
-      )),
-=======
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: RaisedButton(
-        child: Text("asd"),
-        onPressed: () async {},
-      ),
->>>>>>> parent of d94ccb5 (.)
-    );
+    return Scaffold();
   }
 }
