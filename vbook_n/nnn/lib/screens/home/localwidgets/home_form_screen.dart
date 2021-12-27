b39a68e062224d3ac8bd1493e09fab6846f5a4f6
Elif Alim <64834857+ElifYagmurAlim@users.noreply.github.com>
2021-12-27 23:01:32 +0300

    Update home_form_screen.dart

--- vbook_n/nnn/lib/screens/home/localwidgets/home_form_screen.dart
+++ vbook_n/nnn/lib/screens/home/localwidgets/home_form_screen.dart
@@ -14,30 +14,6 @@ class HomeFormScreen extends StatefulWidget {
 
 class _HomeFormScreenState extends State<HomeFormScreen> {
   DatabaseReference dref = FirebaseDatabase.instance.ref();
-//  List<Book> books = [];
-
-//   fechData(DataSnapshot x) {
-// // DatabaseReference ref = FirebaseDatabase.instance.ref();
-//     books.add(Book(
-//         authors: x.child("authors").value.toString(),
-//         average_rating: x.child("average_rating").value.toString(),
-//         bookID: x.child("bookID").value.toString(),
-//         isbn: x.child("isbn").value.toString(),
-//         language_code: x.child("language_code").value.toString(),
-//         num_pages: x.child("num_pages").value.toString(),
-//         publication_date: x.child("publication_date").value.toString(),
-//         publisher: x.child("publisher").value.toString(),
-//         ratings_count: x.child("ratings_count").value.toString(),
-//         text_reviews_count: x.child("text_reviews_count").value.toString(),
-//         title: x.child("title").value.toString(),
-//         isbn13: x.child("isbn13").value.toString()));
-//   }
-
-//   fgetData() {
-//     for (var i = 0; i < books.length; i++) {
-//       print(books[i].title);
-//     }
-//   }
 
   @override
   Widget build(BuildContext context) {
@@ -73,19 +49,6 @@ class _HomeFormScreenState extends State<HomeFormScreen> {
               ));
         },
       ),
-      // body: SafeArea(
-      //     child: FirebaseAnimatedList(
-      //   query: dref,
-      //   itemBuilder: (BuildContext context, DataSnapshot snapshot,
-      //       Animation<double> animation, int index) {
-      //     var x = snapshot;
-      //     fechData(x);
-      //     return ListTile(
-      //       subtitle: Text(books[0].title.toString()),
-      //     );
-      //   },
-      // )
-      // ),
     );
   }
 }
