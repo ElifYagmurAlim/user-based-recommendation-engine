67104d38650854cad67adcbe5911cc609d578ee0
Elif Alim <64834857+ElifYagmurAlim@users.noreply.github.com>
2022-01-05 18:16:28 +0300

    asd

--- vbook_n/nnn/lib/screens/home/localwidgets/recommendation_form_screen.dart
+++ vbook_n/nnn/lib/screens/home/localwidgets/recommendation_form_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_ml_model_downloader/firebase_ml_model_downloader.dart';
 
 class RecommendationFormScreen extends StatefulWidget {
   const RecommendationFormScreen({Key? key}) : super(key: key);
@@ -9,8 +10,25 @@ class RecommendationFormScreen extends StatefulWidget {
 }
 
 class _RecommendationFormScreenState extends State<RecommendationFormScreen> {
+  FirebaseModelDownloader downloader = FirebaseModelDownloader.instance;
+
+  dara() async {
+    FirebaseCustomModel model = await FirebaseModelDownloader.instance
+        .getModel('example_model', FirebaseModelDownloadType.latestModel);
+
+    print('Name: ${model.name}');
+    print('Size: ${model.size}');
+    print('Hash: ${model.hash}');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: MaterialButton(
+        onPressed: () {
+          dara();
+        },
+      ),
+    );
   }
 }
