b6f7b39afc8537f3633d3c19b1202bd7f771d683
Elif Alim <64834857+ElifYagmurAlim@users.noreply.github.com>
2022-01-07 20:53:39 +0300

    asd

--- vbook_n/nnn/lib/screens/home/localwidgets/recommendation_form_screen.dart
+++ vbook_n/nnn/lib/screens/home/localwidgets/recommendation_form_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:firebase_ml_model_downloader/firebase_ml_model_downloader.dart';
+import 'package:flutter/services.dart';
+import 'package:mlkit/mlkit.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
 
 class RecommendationFormScreen extends StatefulWidget {
   const RecommendationFormScreen({Key? key}) : super(key: key);
@@ -11,14 +15,32 @@ class RecommendationFormScreen extends StatefulWidget {
 
 class _RecommendationFormScreenState extends State<RecommendationFormScreen> {
   FirebaseModelDownloader downloader = FirebaseModelDownloader.instance;
+  FirebaseModelInterpreter interpreter = FirebaseModelInterpreter.instance;
+  FirebaseModelManager manager = FirebaseModelManager.instance;
 
   dara() async {
-    FirebaseCustomModel model = await FirebaseModelDownloader.instance
-        .getModel('example_model', FirebaseModelDownloadType.latestModel);
-
-    print('Name: ${model.name}');
-    print('Size: ${model.size}');
-    print('Hash: ${model.hash}');
+    var convertedBytes = new Uint8List(1);
+    var buffer = new ByteData.view(convertedBytes.buffer);
+    manager.registerRemoteModelSource(
+        FirebaseRemoteModelSource(modelName: "example_model"));
+    manager.registerLocalModelSource(FirebaseLocalModelSource(
+        modelName: 'firebase_ml_model',
+        assetFilePath: 'assets/model/firebase_ml_model.tflite'));
+    var results = await interpreter.run(
+        remoteModelName: "example_model",
+        localModelName: "firebase_ml_model",
+        inputOutputOptions: FirebaseModelInputOutputOptions([
+          FirebaseModelIOOption(FirebaseModelDataType.FLOAT32, [1, 1])
+        ], [
+          FirebaseModelIOOption(FirebaseModelDataType.FLOAT32, [1, 1])
+        ]),
+        inputBytes: convertedBytes);
+    // FirebaseCustomModel model = await FirebaseModelDownloader.instance
+    //     .getModel('example_model', FirebaseModelDownloadType.latestModel);
+    print(results);
+    // print('Name: ${model.name}');
+    // print('Size: ${model.size}');
+    // print('Hash: ${model.hash}');
   }
 
   @override
