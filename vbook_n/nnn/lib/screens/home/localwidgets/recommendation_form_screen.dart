e17f02bc0c15a957aab4937ead7eb07d06d504b3
Elif Alim <64834857+ElifYagmurAlim@users.noreply.github.com>
2022-01-11 20:04:56 +0300

    Update recommendation_form_screen.dart

--- vbook_n/nnn/lib/screens/home/localwidgets/recommendation_form_screen.dart
+++ vbook_n/nnn/lib/screens/home/localwidgets/recommendation_form_screen.dart
@@ -98,7 +98,7 @@ class _RecommendationFormScreenState extends State<RecommendationFormScreen> {
             Text(lateargs),
             MaterialButton(
               color: Colors.white,
-              onPressed: () async {
+              onPressed: () {
                 getBooks();
               },
             ),
